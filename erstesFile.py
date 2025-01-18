4ffa45b2a5c9fe92ceb591a83d96d5cbb26b2150
nottangt <62463012+nottangt@users.noreply.github.com>
2025-01-18 23:22:23 +0100

    Curser AI Test

--- erstesFile.py
+++ erstesFile.py
@@ -1,5 +1,29 @@
-import numpy as np
-import matplotlib as plt
+def print_tannenbaum():
+    # Benutzereingabe für das Zeichen und die Anzahl der Reihen
+    zeichen = input("Bitte geben Sie ein Zeichen oder Wort für den Tannenbaum ein: ")
+    reihen = int(input("Bitte geben Sie die Anzahl der Reihen ein: "))
+    
+    zeichen_laenge = len(zeichen)
+    
+    # Für jede Reihe
+    for i in range(reihen):
+        # Berechne die Anzahl der Leerzeichen vor den Zeichen
+        spaces = " " * (zeichen_laenge * (reihen - i - 1))
+        # Berechne die Anzahl der Wiederholungen des Zeichens in dieser Reihe
+        wiederholungen = 2 * i + 1
+        # Drucke die aktuelle Zeile
+        print(spaces + zeichen * wiederholungen)
+    
+    # Füge den Stamm hinzu
+    # Berechne die Mitte des Baums für den Stamm
+    baum_breite = zeichen_laenge * (2 * (reihen - 1) + 1)  # Breite der untersten Baumreihe
+    stamm_breite = int(reihen * 0.5 + 1)
+    stamm_gesamt_breite = stamm_breite * zeichen_laenge
+    stamm_spaces = " " * ((baum_breite - stamm_gesamt_breite) // 2)
+    
+    for i in range(int(reihen * 0.25)):
+        print(stamm_spaces + zeichen * stamm_breite)
 
-erstes_array = np.arange(5)
-print(f'{erstes_array}')
+# Programm ausführen
+if __name__ == "__main__":
+    print_tannenbaum()
